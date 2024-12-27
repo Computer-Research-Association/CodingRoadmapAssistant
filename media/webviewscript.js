48c7da2ce38327fced4bc6c1834403d3c17c846d
dan05s <dan05s@naver.com>
2024-12-27 17:02:19 +0900

    [feat #16] webview design upgrade

--- media/webviewscript.js
+++ media/webviewscript.js
@@ -3,23 +3,23 @@ const vscode = acquireVsCodeApi();
 window.onload = function () {
   function updateList(data, isUserMessage) {
     const list = document.getElementById("list");
-    const li = document.createElement("li");
-    li.textContent = data;
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = data;
 
     if (isUserMessage) {
-      li.style.fontWeight = "bold"; // 사용자 메시지 굵게 표시
+      messageDiv.classList.add("message", "user");
+    } else {
+      messageDiv.classList.add("message", "gpt");
     }
 
-    list.appendChild(li);
-
-    // 대화 내용이 추가된 후 가장 최신 메시지로 스크롤
+    list.appendChild(messageDiv);
     list.scrollTop = list.scrollHeight;
   }
 
   window.addEventListener("message", (event) => {
     const message = event.data;
     if (message.command === "setData") {
-      updateList(message.data);
+      updateList(message.data, false);
     }
   });
 
@@ -28,14 +28,14 @@ window.onload = function () {
     const inputValue = inputElement.value.trim();
 
     if (inputValue) {
-      updateList(inputValue, true); // 사용자의 메시지
+      updateList(inputValue, true);
       vscode.postMessage({ command: "process", value: inputValue });
-      inputElement.value = ""; // 입력 필드 비우기
+      inputElement.value = "";
     }
   });
 
   document.getElementById("reset").addEventListener("click", () => {
     const list = document.getElementById("list");
-    list.innerHTML = ""; // 모든 대화 내용 지우기
+    list.innerHTML = "";
   });
 };
