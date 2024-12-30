b03278839b0d347142acecd9352161dab76db0b6
dan05s <dan05s@naver.com>
2024-12-30 13:55:01 +0900

    [feat #21] Move webview to activitybar and implement navbar

--- media/webviewscript.js
+++ media/webviewscript.js
@@ -1,41 +1,20 @@
-const vscode = acquireVsCodeApi();
+const addStepBtn = document.getElementById("add-step-btn");
+const submitBtn = document.getElementById('submit-btn');
 
-window.onload = function () {
-  function updateList(data, isUserMessage) {
-    const list = document.getElementById("list");
-    const li = document.createElement("li");
-    li.textContent = data;
+let stepCount = 1;
 
-    if (isUserMessage) {
-      li.style.fontWeight = "bold"; // 사용자 메시지 굵게 표시
-    }
+function addNewStep() {
+  console.log("함수 실행"); 
+  const newInput = document.createElement("input");
+  newInput.type = "text";
+  newInput.placeholder = `Step ${stepCount + 1}`;
+  newInput.id = `step-${stepCount + 1}`;
+  newInput.className = "step-btn";
+  
+  const newInputContainer = document.getElementById("input-container");
+  newInputContainer.appendChild(newInput); 
+  newInput.focus();
+  stepCount++;
+}
 
-    list.appendChild(li);
-
-    // 대화 내용이 추가된 후 가장 최신 메시지로 스크롤
-    list.scrollTop = list.scrollHeight;
-  }
-
-  window.addEventListener("message", (event) => {
-    const message = event.data;
-    if (message.command === "setData") {
-      updateList(message.data);
-    }
-  });
-
-  document.getElementById("send").addEventListener("click", () => {
-    const inputElement = document.getElementById("input");
-    const inputValue = inputElement.value.trim();
-
-    if (inputValue) {
-      updateList(inputValue, true); // 사용자의 메시지
-      vscode.postMessage({ command: "process", value: inputValue });
-      inputElement.value = ""; // 입력 필드 비우기
-    }
-  });
-
-  document.getElementById("reset").addEventListener("click", () => {
-    const list = document.getElementById("list");
-    list.innerHTML = ""; // 모든 대화 내용 지우기
-  });
-};
+addStepBtn.addEventListener('click', addNewStep);
