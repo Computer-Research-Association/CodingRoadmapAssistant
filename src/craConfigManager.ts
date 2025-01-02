98a1b8b8c96328ba1aed38918751db0c9900e68b
Seongbin Kim <93897642+young170@users.noreply.github.com>
2025-01-02 17:21:25 +0900

    [feat] reload webview on openai api key update

--- src/craConfigManager.ts
+++ src/craConfigManager.ts
@@ -20,6 +20,10 @@ export async function setAPIKey(context: vscode.ExtensionContext) {
 
   await vscode.workspace.getConfiguration().update("openAI.apiKey", apiKey, vscode.ConfigurationTarget.Global);
   vscode.window.showInformationMessage("Open AI API key saved successfully.");
+
+  // refresh webview to apply openai api key
+  vscode.commands.executeCommand("workbench.action.webview.reloadWebviewAction");
+
   return apiKey;
 }
 
