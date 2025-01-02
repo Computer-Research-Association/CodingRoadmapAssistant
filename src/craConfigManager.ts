0b71446ed731abf0363e2046d8669945bc5ff934
chachaaa-23 <yuzuyuzu2004@naver.com>
2025-01-02 15:13:12 +0900

    [init #25] workspace folder 생성, workspace path 생성

--- src/craConfigManager.ts
+++ src/craConfigManager.ts
@@ -37,16 +37,32 @@ export async function showModelSelectionQuickPick() {
 export async function initConversationLogFile(context: vscode.ExtensionContext) {
   let workspacePath = "";
 
-  const data = [
-    {
-      role: "system",
-      content:
-        "You are my coding assistant to enhance my coding skills. From now on, i’ll give you some coding problem and user’s attempt to solve it. Also you may be given logical steps of the user’s code.",
-    },
-    {
-      role: "system",
-      content:
-        "Generate the output that includes the following points. 1. Never give the answer. Including code. 2. tutor him. must ask questions. make sure the user understands it themselves. 3. for each logical step, provide three questions made in step 2. number each question 1, 2, 3 for each logical step. ",
-    },
-  ];
+  try {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+
+    //workdpace folder check, path 생성
+    if (workspaceFolders && workspaceFolders.length > 0) {
+      workspacePath = workspaceFolders[0].uri.fsPath; // 한 개의 워크스페이스 경로만 가져옴(추후 여러 개의 경로 구현 필요.)
+      console.log("workspace folder exist! " + `workspace path: ${workspacePath}`);
+    } else {
+      console.log("no workspace Folder exist");
+    }
+    fs.mkdir(workspacePath + "/out", (err) => console.log(err)); //폴더 만들기
+
+    const data = [
+      {
+        role: "system",
+        content:
+          "You are my coding assistant to enhance my coding skills. From now on, i’ll give you some coding problem and user’s attempt to solve it. Also you may be given logical steps of the user’s code.",
+      },
+      {
+        role: "system",
+        content:
+          "Generate the output that includes the following points. 1. Never give the answer. Including code. 2. tutor him. must ask questions. make sure the user understands it themselves. 3. for each logical step, provide three questions made in step 2. number each question 1, 2, 3 for each logical step. ",
+      },
+    ];
+  } catch {
+    console.log("Error happened at making files : ");
+    throw new Error();
+  }
 }
