23ae6d0d8904040255b9945afe588be5786bb843
Seongbin Kim <93897642+young170@users.noreply.github.com>
2025-01-02 17:22:28 +0900

    [feat] reload webview on openai gpt model update

--- src/craConfigManager.ts
+++ src/craConfigManager.ts
@@ -34,6 +34,9 @@ export async function showModelSelectionQuickPick() {
       await vscode.workspace.getConfiguration().update("openAI.modelSelected", item, vscode.ConfigurationTarget.Global);
     },
   });
+
+  // refresh webview to apply openai gpt model
+  vscode.commands.executeCommand("workbench.action.webview.reloadWebviewAction");
 }
 
 export async function onFirstActivation(context: vscode.ExtensionContext) {
