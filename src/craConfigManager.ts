f29a71a2fcda77b86d04968edf21b8d067aa7b90
chachaaa-23 <yuzuyuzu2004@naver.com>
2025-01-08 14:21:07 +0900

    [fix #41] add prompt message - adding codes

--- src/craConfigManager.ts
+++ src/craConfigManager.ts
@@ -66,12 +66,12 @@ export async function showApiKeyError(context: vscode.ExtensionContext) {
 export async function initPromptMessage(context: vscode.ExtensionContext) {
   const data = [
     {
-      role: "assistant",
+      role: "system",
       content:
-        "You are my coding assistant to enhance my coding skills. From now on, I’ll give you some coding problems and the user’s attempts to solve them. Also you may be given logical steps of the user’s code.",
+        "You are my coding assistant to enhance my coding skills. From now on, I’ll give you some coding problems, the user’s attempts and user's code to solve them. Also you may be given logical steps of the user’s code.",
     },
     {
-      role: "assistant",
+      role: "system",
       content:
         "Generate the output that includes the following points. 1. Never give the answer. Including code. 2. tutor him. must ask questions. make sure the user understands it themselves. 3. for each logical step, provide three questions made in step 2. number each question 1, 2, 3 for each logical step. ",
     },
