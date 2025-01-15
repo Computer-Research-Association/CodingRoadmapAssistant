67c32a1646583503e264314df3fdc84cd61d20eb
Jangmyun <cndlsrb2739@gmail.com>
2025-01-15 10:18:45 +0900

    [style #58] chat style

---
+++ webview-ui/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="title">Coding Roadmap Assistant</div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
