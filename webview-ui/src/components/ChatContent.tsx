facce380f79ead688e61769ba2b327b36784e2bc
chachaaa-23 <yuzuyuzu2004@naver.com>
2025-01-16 14:35:12 +0900

    [feat #61] 클릭 시 테두리 표시 및 icon 표시

--- webview-ui/src/components/ChatContent.tsx
+++ webview-ui/src/components/ChatContent.tsx
@@ -2,28 +2,43 @@ import useMessagesStore from "../stores/messagesStore";
 import "../styles/ChatContent.css";
 import { VscTrash } from "react-icons/vsc";
 import { VscEdit } from "react-icons/vsc";
+import { useState } from "react";
 
 function ChatContent() {
   const { messages, deleteMessage } = useMessagesStore();
+  const [selectedBoxIndex, setSelectedBoxIndex] = useState<number | null>(null);
+
+  const handleBoxClick = (index: number) => {
+    setSelectedBoxIndex(index);
+  };
+
+  const handleOtherClick = () => {
+    setSelectedBoxIndex(null);
+  };
 
   return (
-    <main id="chat-container">
+    <main id="chat-container" onClick={handleOtherClick}>
       {messages && messages.length > 0 ? (
-        <div className="messages-container flex">
+        <div className="messages-container flex" onClick={(e) => e.stopPropagation()}>
           {messages.map((message, index) => (
-            <div key={message.type + index} className="message-box">
+            <div
+              key={message.type + index}
+              className={`message-box  ${selectedBoxIndex === index ? " selected" : ""}`}
+              onClick={() => handleBoxClick(index)}>
               <div className="message-text">
                 <div className="message-type">{index === 0 ? message.type : `${message.type} ${index}`}</div>
                 <div className="message-content">{message.content}</div>
               </div>
-              <div className="message-icon">
-                <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
-                  <VscTrash />
+              {selectedBoxIndex === index && (
+                <div className="message-icon">
+                  <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
+                    <VscTrash />
+                  </div>
+                  <div className="message-icon-edit">
+                    <VscEdit />
+                  </div>
                 </div>
-                <div className="message-icon-edit">
-                  <VscEdit />
-                </div>
-              </div>
+              )}
             </div>
           ))}
         </div>
