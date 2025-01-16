7b11909a3643e33315514aa93715da634a495936
chachaaa-23 <yuzuyuzu2004@naver.com>
2025-01-16 10:27:43 +0900

    [feat #61] add messageBox border, step

--- webview-ui/src/components/ChatContent.tsx
+++ webview-ui/src/components/ChatContent.tsx
@@ -1,6 +1,10 @@
 import useMessagesStore from "../stores/messagesStore";
+import "../styles/ChatContent.css";
+
 function ChatContent() {
   const { messages } = useMessagesStore();
+
+  const stepIndex = 1;
 
   return (
     <main id="chat-container">
@@ -8,7 +12,7 @@ function ChatContent() {
         <div className="messages-container">
           {messages.map((message, index) => (
             <div key={message.type + index} className="message-box">
-              <div className="message-type">{message.type}</div>
+              <div className="message-type">{index === 0 ? message.type : `${stepIndex}. ${message.type}`}</div>
               <div className="message-content">{message.content}</div>
             </div>
           ))}
