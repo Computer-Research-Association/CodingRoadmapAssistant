d0fa466ab9e3728a2a4ae634943318d2277a8277
chachaaa-23 <yuzuyuzu2004@naver.com>
2025-01-16 12:06:27 +0900

    [feat #61] connect clearMessages to icon

--- webview-ui/src/components/Header.tsx
+++ webview-ui/src/components/Header.tsx
@@ -1,14 +1,16 @@
 import "../styles/Header.css";
+import useMessagesStore from "../stores/messagesStore";
 import { AiOutlineClear } from "react-icons/ai";
 import { VscHistory } from "react-icons/vsc";
 
 function Header() {
+  const { clearMessages } = useMessagesStore();
   return (
     <>
       <div className="header flex">
         <div className="title">Chat with CRA</div>
         <div className="icon flex">
-          <div className="icon-clear">
+          <div className="icon-clear" onClick={clearMessages}>
             <AiOutlineClear />
           </div>
           <div className="icon-history">
