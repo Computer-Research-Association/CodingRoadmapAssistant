a7b612acd594a2b0c7083d6ac9d77338a494b017
Jangmyun <cndlsrb2739@gmail.com>
2025-01-21 20:09:40 +0900

    [refactor #70] input 요소가 focus out 됐을 때 요소를 css에서 다루도록 변경

--- webview-ui/src/components/ChatContent.tsx
+++ webview-ui/src/components/ChatContent.tsx
@@ -1,37 +1,21 @@
 import useMessagesStore from "../stores/messagesStore";
 import "../styles/ChatContent.css";
 import { VscTrash } from "react-icons/vsc";
-import React, { useState } from "react";
+import React from "react";
 
 function ChatContent() {
   const { messages, deleteMessage, updateMessage } = useMessagesStore();
-  const [selectedBoxIndex, setSelectedBoxIndex] = useState<number | null>(null);
 
-  // 클릭한 박스의 인덱스, set
-  const handleBoxClick = (index: number) => {
-    setSelectedBoxIndex(index);
-  };
-
-  const handleOtherClick = () => {
-    setSelectedBoxIndex(null);
-  };
-
-  // update message when focus out, initialize index
   const handleBlur = (e: React.ChangeEvent<HTMLDivElement>, index: number) => {
     updateMessage(index, e.target.innerText);
-    setSelectedBoxIndex(null); // 편집 종료
   };
 
   return (
-    <main id="chat-container" onClick={handleOtherClick}>
+    <main id="chat-container">
       {messages && messages.length > 0 ? (
         <div className="messages-container flex" onClick={(e) => e.stopPropagation()}>
           {messages.map((message, index) => (
-            <div
-              key={message.type + index}
-              className={`message-box  ${selectedBoxIndex === index ? " selected" : ""}`}
-              onClick={() => handleBoxClick(index)} // 특정 박스 클릭 시, index update
-            >
+            <div key={message.type + index} className={`message-box`}>
               <div className="message-text">
                 <div className="message-type">{index === 0 ? message.type : `${message.type} ${index}`}</div>
                 <div
@@ -42,14 +26,11 @@ function ChatContent() {
                   {message.content}
                 </div>
               </div>
-
-              {selectedBoxIndex === index && ( // when user clicked
-                <div className="message-icon">
-                  <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
-                    <VscTrash />
-                  </div>
+              <div className="message-icon">
+                <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
+                  <VscTrash />
                 </div>
-              )}
+              </div>
             </div>
           ))}
         </div>
