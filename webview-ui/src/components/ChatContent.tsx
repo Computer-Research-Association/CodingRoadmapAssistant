e037f63eca4ee437b77f6a0955b4dea4b02f15cd
Jangmyun <cndlsrb2739@gmail.com>
2025-01-21 20:42:53 +0900

    [refactor #70] MessageBox를 컴포넌트로 분리

--- webview-ui/src/components/ChatContent.tsx
+++ webview-ui/src/components/ChatContent.tsx
@@ -2,9 +2,10 @@ import useMessagesStore from "../stores/messagesStore";
 import "../styles/ChatContent.css";
 import { VscTrash } from "react-icons/vsc";
 import React from "react";
+import { Message } from "../types/messageStoreTypes";
 
 function ChatContent() {
-  const { messages, deleteMessage, updateMessage } = useMessagesStore();
+  const { messages, updateMessage } = useMessagesStore();
 
   const handleBlur = (e: React.ChangeEvent<HTMLDivElement>, index: number) => {
     updateMessage(index, e.target.innerText);
@@ -15,29 +16,45 @@ function ChatContent() {
       {messages && messages.length > 0 ? (
         <div className="messages-container flex" onClick={(e) => e.stopPropagation()}>
           {messages.map((message, index) => (
-            <div key={message.type + index} className={`message-box`}>
-              <div className="message-text">
-                <div className="message-type">{index === 0 ? message.type : `${message.type} ${index}`}</div>
-                <div
-                  className="message-content"
-                  contentEditable={true}
-                  onBlur={(e) => handleBlur(e, index)} // input 입력받고, focus out 시 값 update
-                >
-                  {message.content}
-                </div>
-              </div>
-              <div className="message-icon">
-                <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
-                  <VscTrash />
-                </div>
-              </div>
-            </div>
+            <MessageBox key={message.type + index} message={message} index={index} handleBlur={handleBlur} />
           ))}
         </div>
       ) : (
         <div className="learnCRA">LEARN CRA!!!</div>
       )}
     </main>
+  );
+}
+
+function MessageBox({
+  handleBlur,
+  index,
+  message,
+}: {
+  handleBlur: (e: React.ChangeEvent<HTMLDivElement>, index: number) => void;
+  index: number;
+  message: Message;
+}) {
+  const { deleteMessage } = useMessagesStore();
+
+  return (
+    <div className="message-box">
+      <div className="message-text">
+        <div className="message-type">{index === 0 ? message.type : `${message.type} ${index}`}</div>
+        <div
+          className="message-content"
+          contentEditable={true}
+          onBlur={(e) => handleBlur(e, index)} // input 입력받고, focus out 시 값 update
+        >
+          {message.content}
+        </div>
+      </div>
+      <div className="message-icon">
+        <div className="message-icon-trash" onClick={() => deleteMessage(index)}>
+          <VscTrash />
+        </div>
+      </div>
+    </div>
   );
 }
 
