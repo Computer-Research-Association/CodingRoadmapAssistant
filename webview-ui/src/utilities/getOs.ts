fc988e76b71531bfce7646cbd5b7d9d785575ed7
Jangmyun <cndlsrb2739@gmail.com>
2025-01-23 14:25:42 +0900

    [chore #74] os 확인에 window.navigator.userAgent 정보를 사용

--- webview-ui/src/utilities/getOs.ts
+++ webview-ui/src/utilities/getOs.ts
@@ -1,5 +1,5 @@
 export default function getOs() {
-  const platform = window.navigator.platform.toLowerCase();
+  const platform = window.navigator.userAgent.toLowerCase();
   if (platform.includes("mac")) return "mac";
   if (platform.includes("win")) return "windows";
   if (platform.includes("linux")) return "linux";
