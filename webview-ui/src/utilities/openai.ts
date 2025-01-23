9f719e226cebf8d87e773aceec5184d3291c735e
Jangmyun <cndlsrb2739@gmail.com>
2025-01-23 10:44:05 +0900

    [feat #74] openai API 요청하도록 extension에 message전달하는 오브젝트

--- webview-ui/src/utilities/openai.ts
+++ webview-ui/src/utilities/openai.ts
@@ -1,5 +1,15 @@
+import { vscode } from "./vscode";
 import { Message } from "../types/messageStoreTypes";
 
 export const combineMessages = (messages: Message[]): string => {
   return messages.map((message, i) => `${i === 0 ? "Definition" : i} ${message.content}`).join("\n");
+};
+
+export const openai = {
+  sendMessage: (message: string) => {
+    vscode.postMessage({
+      command: "process",
+      value: message,
+    });
+  },
 };
