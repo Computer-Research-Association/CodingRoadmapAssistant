00f4b6d5e2d24de44aa038b9c9e3248694d56351
Jangmyun <cndlsrb2739@gmail.com>
2025-01-23 14:12:36 +0900

    [feat #74] 현재 기기의 os를 리턴하는 함수

---
+++ webview-ui/src/utilities/getOs.ts
@@ -0,0 +1,7 @@
+export default function getOs() {
+  const platform = window.navigator.platform.toLowerCase();
+  if (platform.includes("mac")) return "mac";
+  if (platform.includes("win")) return "windows";
+  if (platform.includes("linux")) return "linux";
+  return "unknown";
+}
