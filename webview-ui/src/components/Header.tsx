63e3db9ac6a119737a829de5cbaf547855ec855d
Jangmyun <cndlsrb2739@gmail.com>
2025-02-05 15:52:12 +0900

    [feat #97] log history button click handler 선언

--- webview-ui/src/components/Header.tsx
+++ webview-ui/src/components/Header.tsx
@@ -2,9 +2,16 @@ import "../styles/Header.css";
 import useMessagesStore from "../stores/messagesStore";
 import { AiOutlineClear } from "react-icons/ai";
 import { VscHistory } from "react-icons/vsc";
+import { vscode } from "../utilities/vscode";
 
 function Header() {
   const { clearMessages } = useMessagesStore();
+  const handleHistoryClick = () => {
+    vscode.postMessage({
+      command: "history",
+    });
+  };
+
   return (
     <>
       <div className="header flex">
@@ -13,7 +20,7 @@ function Header() {
           <div className="icon-clear" onClick={clearMessages}>
             <AiOutlineClear />
           </div>
-          <div className="icon-history">
+          <div className="icon-history" onClick={handleHistoryClick}>
             <VscHistory />
           </div>
         </div>
