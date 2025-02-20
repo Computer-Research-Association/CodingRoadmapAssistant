20372adc9127e0a6927e8d5c657e0ddedd9f2c0e
Jangmyun <cndlsrb2739@gmail.com>
2025-02-20 16:50:52 +0900

    [feat #143] SettingWindow 컴포넌트 toggle flag 선언

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import ChatInput from "./components/ChatInput";
 import ChatContent from "./components/ChatContent";
+import SettingWindow from "./components/SettingWindow";
 
 function App() {
+  const [isSettingWindowOpened, setIsSettingWindowOpened] = useState(false);
+
   return (
     <div id="chat">
-      <Header />
+      <Header onSettingClick={() => setIsSettingWindowOpened(true)} />
       <ChatContent />
       <ChatInput />
+      <SettingWindow onClose={() => setIsSettingWindowOpened(false)} isOpened={isSettingWindowOpened} />
     </div>
   );
 }
