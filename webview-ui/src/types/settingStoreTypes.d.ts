b46afb9091f7ed5ea536375411d313996ca17518
Jangmyun <cndlsrb2739@gmail.com>
2025-02-22 17:25:53 +0900

    [feat #147] options state관리를 위한 zustand store 선언

--- webview-ui/src/types/settingStoreTypes.d.ts
+++ webview-ui/src/types/settingStoreTypes.d.ts
@@ -1,4 +1,11 @@
 export interface SettingState {
   language: string;
+  apiKey: string;
+  gptModel: string;
+
   setLanguage: (language: string) => void;
+  setApiKey: (apiKey: string) => void;
+  setGptModel: (model: string) => void;
+  resetSettings: () => void;
+  initializeSettings: (language: string, apiKey: string, gptModel: string) => void;
 }
