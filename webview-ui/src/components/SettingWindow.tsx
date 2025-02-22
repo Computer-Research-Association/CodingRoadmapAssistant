cebfa30d7156fcaa55bb7ed243bdd52ba6a58a3d
Jangmyun <cndlsrb2739@gmail.com>
2025-02-22 17:30:56 +0900

    [feat #147] SettingWindow에 세팅 옵션을 설정할 수 있는 select, input 추가

--- webview-ui/src/components/SettingWindow.tsx
+++ webview-ui/src/components/SettingWindow.tsx
@@ -1,12 +1,23 @@
-import { VscClose } from "react-icons/vsc";
+import useSettingStore from "../stores/settingStore";
 import "../styles/SettingWindow.css";
 import commonStyles from "../styles/common.module.css";
+import { vscode } from "../utilities/vscode";
+import { VscEye, VscEyeClosed } from "react-icons/vsc";
+import { VscClose } from "react-icons/vsc";
+import { useState } from "react";
 
 function SettingWindow({ onClose, isOpened }: { onClose: () => void; isOpened: boolean }) {
+  const [apiKeyShow, setApiKeyShow] = useState(false);
+  const handleApiKeyShow = () => {
+    setApiKeyShow(!apiKeyShow);
+  };
+
+  const { language, apiKey, gptModel, setApiKey, setLanguage, setGptModel } = useSettingStore();
+
   if (!isOpened) return null;
 
   return (
-    <div className="setting-overlay">
+    <div className="setting-overlay" draggable={false}>
       <div className="setting-window">
         <div className="close-container">
           <div className="title">Setting</div>
@@ -14,7 +25,59 @@ function SettingWindow({ onClose, isOpened }: { onClose: () => void; isOpened: b
             <VscClose />
           </div>
         </div>
-        <div className="settings">{/* 세팅 옵션들이 들어와야 할 자리입니다. */}</div>
+
+        <div className="settings">
+          <label htmlFor="language">
+            <span>Language</span>
+            <select
+              name="language"
+              id="language"
+              value={language}
+              className={commonStyles.select}
+              onChange={(e) => setLanguage(e.target.value)}>
+              <option value="en">English</option>
+              <option value="kr">Korean</option>
+              <option value="jp">Japanese</option>
+              <option value="es">Spanish</option>
+              <option value="cn">Chinese</option>
+            </select>
+          </label>
+
+          <label htmlFor="model">
+            <span>Model</span>
+            <select
+              name="model"
+              id="model"
+              value={gptModel}
+              className={commonStyles.select}
+              onChange={(e) => setGptModel(e.target.value)}>
+              <option value="gpt-4o-mini">gpt-4o-mini</option>
+              <option value="gpt-4o">gpt-4o</option>
+              <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+            </select>
+          </label>
+
+          <label htmlFor="apiKey">
+            <span>API Key</span>
+            <div className="input-wrapper">
+              <input
+                type={apiKeyShow ? "text" : "password"}
+                value={apiKey}
+                onChange={(e) => setApiKey(e.target.value)}
+                name="apiKey"
+                id="apiKey"
+                className="input"
+              />
+              <div className={commonStyles.icon} onClick={handleApiKeyShow}>
+                {apiKeyShow ? <VscEye /> : <VscEyeClosed />}
+              </div>
+            </div>
+          </label>
+
+          <button onClick={() => vscode.postMessage({ command: "save-settings" })} className={commonStyles.button}>
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
